f625ba0325c422b4f5cb3531d7ae0d810b08bc77
Alex Gomes <alex.gomes.net3@gmail.com>
2020-04-27 19:43:58 -0300

    Criando um novo Controller e Rota para autenticação de usuários com e-mail e password.

--- start/routes.js
+++ start/routes.js
@@ -3,4 +3,8 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
+// Created new user
 Route.post('users', 'UserController.store')
+
+// Auth users in system
+Route.post('sessions', 'SessionController.store')
